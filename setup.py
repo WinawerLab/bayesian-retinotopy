f6b271e9b5fb8ad08478654d5bee67b72a6c3a0b
noahbenson <nben@nyu.edu>
2017-08-24 11:39:06 -0400

    updated repository name, website, etc

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ from setuptools import setup
 version = '0.0.1'
 
 setup(
-    name='v123-retinotopic-maps',
+    name='bayesian-retinotopy',
     version=version,
-    description='Analysis repository that is the companion of the paper Benson et al. (2017)',
+    description='Analysis repository that is the companion of the paper Benson and Winawer (2017)',
     keywords='neuroscience mesh cortex registration',
     author='Noah C. Benson',
     author_email='nben@nyu.edu',
     maintainer_email='nben@nyu.edu',
     long_description='''
                      See the README.md file at the github repository for this package:
-                     https://github.com/winawerlab/v123-retinotopic-maps
+                     https://github.com/winawerlab/bayesian-retinotopy
                      ''',
-    url='https://github.com/winawerlab/v123-retinotopic-maps',
-    download_url='https://github.com/winawerlab/v123-retinotopic-maps',
+    url='https://github.com/winawerlab/bayesian-retinotopy',
+    download_url='https://github.com/winawerlab/bayesian-retinotopy',
     license='GPLv3',
     classifiers=['Intended Audience :: Science/Research',
                  'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
@@ -33,6 +33,6 @@ setup(
     packages=['v123'],
     include_package_data=True,
     package_data={'': ['LICENSE.txt']},
-    install_requires=['neuropythy>=0.2',
+    install_requires=['neuropythy>=0.3',
                       'numpy>=1.2',
                       'scipy>=0.7'])
